c8c28d6f39c33513fcbd7433aa54fd9056d11f7d
caesar <Caesar_Linsa@163.com>
2019-04-21 02:02:01 +0800

    add function description

--- python/ConfigParse.py
+++ python/ConfigParse.py
@@ -5,23 +5,27 @@ class ConfigParse(object):
     def __init__(self, file):
         self.file = file
         self.conf = ConfigParser()
-
+    
+    # read ini file to dict
     def read_file(self):
         self.conf.read(self.file)
         return { section:dict(self.conf.items(section)) for section in self.conf.sections()}
 
+    # write dict to ini file
     def write_file(self, di):
         section_data = self.read_file()
         section_remove = [ d for d in section_data.keys() if d not in di.keys()]
         section_add = [ s for s in di.keys() if s not in section_data.keys() ]
         for k in section_remove:
             self.conf.remove_section(k)
+        # set option ,value to ini file according to the dict
         for k in di.keys():
              if k in section_add:
                  self.conf.add_section(k)
              for option,value in di.get(k).items():
                  self.conf.set(k,option,value)
 
+        # remove the option that not in dict
         section_have = [ k for k in section_data.keys() if k not in section_remove]
         for k in section_have:
             for option,value in section_data.get(k).items():
