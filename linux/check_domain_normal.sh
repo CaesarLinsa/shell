5cbb4a2f373c33339ee32bb9e3e5e824d486b2d7
caesar <Caesar_Linsa@163.com>
2019-07-14 05:10:28 +1000

    update fuction to check file modify time less than 3600
    1.update fuction to check file modify time less than 3600 seconds
    2.cat file to find key words error info

--- linux/check_domain_normal.sh
+++ linux/check_domain_normal.sh
@@ -1,13 +1,38 @@
 source log.sh
-domain="https://100.100.199.100:500/caesar/cast"
-domain_ip=$(echo $domain |sed "{s/[^/]*\/\///;s/:.*//}")
-ping_info=$(ping $domain_ip -c 4)
-sa=$(echo "$ping_info"|grep -v "100% packet loss"|grep "0% packet loss")
-if [ -z "$sa" ]
+
+check_domain_normal(){
+    # domain=https://www.baidu.com:8090/caesar/rest
+    local domain="$1"
+    domain_ip=$(echo $domain |sed "{s/[^/]*\/\///;s/:.*//}")
+    ping_info=$(ping $domain_ip -c 4)
+    sa=$(echo "$ping_info"|grep -v "100% packet loss"|grep "0% packet loss")
+    if [ -z "$sa" ]
+      then
+       log_error "ping  $domain_ip is unreachable"
+      else
+       ip=$(echo "$ping_info" |grep "time=" |sed '{s/[^(]*(//;s/).*//;q}')
+       log_info "ping $domain_ip is  $ip  normal"
+    fi
+}
+
+check_file_modify_time(){
+ local file=$1 
+ file_time=$(stat -c %Y $file)
+ ctime=$(date +%s)
+ if [ $(($ctime - $file_time)) -ge 3600 ]
+ then
+   log_info "test.sh is modify more than 3600 seconds"
+ fi 
+}
+
+check_file_exist_key_words(){
+  local file="$1"
+  local key_word="$2"
+  chour=$(date +"%Y-%m-%dT%H")
+  error_infos=$(cat $file |grep "$chour"|grep $key_word)
+  if [ ! -z "$error_infos" ]
   then
-   log_error "ping  $domain_ip is unreachable"
-  else
-   ip=$(echo "$ping_info" |grep "time=" |sed '{s/[^(]*(//;s/).*//;q}')
-   log_info "ping $domain_ip is  $ip  normal"
-fi
+    log_error "$error_infos"
+  fi
+}
 
