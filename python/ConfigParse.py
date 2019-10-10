1b481dd6cd7586b51a6c3c290236f1b6e8f31808
caesar <Caesar_Linsa@163.com>
2019-10-11 05:03:51 +1100

    dd ini2json function and command line for ConfigParse such as: python ConfigParse.py caesar.ini ini2json

--- python/ConfigParse.py
+++ python/ConfigParse.py
@@ -1,5 +1,7 @@
 from ConfigParser import RawConfigParser as ConfigParser
-### test
+import sys
+import json
+
 
 class ConfigParse(object):
 
@@ -10,48 +12,53 @@ class ConfigParse(object):
     # read ini file to dict
     def read_file(self):
         self.conf.read(self.file)
-        return { section:dict(self.conf.items(section)) for section in self.conf.sections()}
+        return {section: dict(self.conf.items(section))
+                for section in self.conf.sections()}
+
+    def ini2json(self):
+        json_data = self.read_file()
+        return json.dumps(json_data, indent=4, sort_keys=True)
 
     # write dict to ini file
     def write_file(self, di):
         dic = self._dic_to_ini_validate(di) 
         section_data = self.read_file()
-        section_remove = [ d for d in section_data.keys() if d not in dic.keys()]
-        section_add = [ s for s in dic.keys() if s not in section_data.keys() ]
+        section_remove = [d for d in section_data.keys() if d not in dic.keys()]
+        section_add = [s for s in dic.keys() if s not in section_data.keys()]
         for k in section_remove:
             self.conf.remove_section(k)
         # set option ,value to ini file according to the dict
         for k in di.keys():
-             if k in section_add:
-                 self.conf.add_section(k)
-             for option,value in di.get(k).items():
-                 self.conf.set(k,option,value)
+            if k in section_add:
+                self.conf.add_section(k)
+            for option, value in di.get(k).items():
+                self.conf.set(k, option, value)
 
         # remove the option that not in dict
-        section_have = [ k for k in section_data.keys() if k not in section_remove]
+        section_have = [k for k in section_data.keys() if k not in section_remove]
         for k in section_have:
-            for option,value in section_data.get(k).items():
+            for option, value in section_data.get(k).items():
                 if not dic.get(k).get(option, None):
                     self.conf.remove_option(k, option)
 
-        with open(self.file,'w') as f:
+        with open(self.file, 'w') as f:
             self.conf.write(f)
 
     def _dic_to_ini_validate(self, dic):
         complex_types = [dict, list, tuple]
         if not isinstance(dic, dict):
-            raise Exception("%s is not valid dict" %dic)
+            raise Exception("%s is not valid dict" % dic)
         for section, value in dic.items():
             if type(section) in complex_types:
-                raise Exception("section %s must be str" %section)
+                raise Exception("section %s must be str" % section)
             if not isinstance(value, dict):
-                raise Exception("%s is not valid dict" %dic)
-            for k,v in value.items():
+                raise Exception("%s is not valid dict" % dic)
+            for k, v in value.items():
                 if type(k) in complex_types:
-                    raise Exception("option %s must be str" %k)
+                    raise Exception("option %s must be str" % k)
                 if type(v) in complex_types:
-                    raise Exception("value %s must be str" %v)
-                value[str(k)]=str(value.pop(k))
+                    raise Exception("value %s must be str" % v)
+                value[str(k)] = str(value.pop(k))
             dic[str(section)] = dic.pop(section)
         return dic
     
@@ -62,7 +69,7 @@ class ConfigParse(object):
         if option in options:
             dic[option][key] = value
         else:    
-            dic[option] = {key:value}
+            dic[option] = {key: value}
         self.write_file(dic)
 
     # del the key that in the option
@@ -97,8 +104,7 @@ class ConfigParse(object):
         dic = self.read_file()
         if option not in dic.keys():
             return []
-        else:
-           return dic[option].keys()
+        return dic[option].keys()
    
     def get_option_key_value(self, option, key):
         dic = self.read_file()
@@ -109,3 +115,29 @@ class ConfigParse(object):
                 return
             else:
                 return dic[option][key]
+
+
+def print_help():
+    print("Usage: python ConfigParse.py \n"
+          "              ini-file \n"
+          "              function-name \n"
+          "              function-args")
+    print("       ini-file:ini file path such as caesar.ini")
+    print("       function-name: the function name in ConfigParse")
+    print("       function-args: the function args of function-name")
+
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    if any(["--help" in argvs, "-h" in argvs,
+           len(argvs) < 3]):
+        print_help()
+        exit(0)
+    file_path = argvs[1]
+    function_name = argvs[2]
+    cf = ConfigParse(file_path)
+    if len(argvs) == 3:
+        print(getattr(cf, function_name)())
+    else:
+        function_args = argvs[3:]
+        print(getattr(cf, function_name)(*function_args))
